0975c61c4f53db3eccefe36e405b3afeb082c318
Kilian <k.allaire@rubika-edu.com>
2019-04-18 16:35:31 +0200

    Shit load of improvement

---
+++ Testing/Assets/Kilian/Script/GridLess/GridLess_Surface_Delegate.cs
@@ -0,0 +1,6 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public delegate Vector3 GridLess_Surface_Delegate (float u, float v, float t);
+
--- Testing/Assets/Script/Grid/Math_Surface_Delegate.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public delegate float Math_Surfaces_Delegate (float x, float z, float t);
-
